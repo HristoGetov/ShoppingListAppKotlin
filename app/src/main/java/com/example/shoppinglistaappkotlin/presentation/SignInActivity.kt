f95f858c0075a08c1142f7b559594f02edb1368f
Hristo <mr.hristo.getov@gmail.com>
2023-07-08 15:38:26 +0300

    Sign in with Google acount added

--- app/src/main/java/com/example/shoppinglistaappkotlin/presentation/SignInActivity.kt
+++ app/src/main/java/com/example/shoppinglistaappkotlin/presentation/SignInActivity.kt
@@ -1,17 +1,28 @@
 package com.example.shoppinglistaappkotlin.presentation
 
+import android.app.Activity
+import android.content.ContentProviderClient
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.shoppinglistaappkotlin.R
 import com.example.shoppinglistaappkotlin.databinding.ActivitySignInBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +30,16 @@ class SignInActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
 
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        binding.googleSignIn.setOnClickListener{
+            signInGoogle()
+        }
         binding.textView.setOnClickListener{
             val intent = Intent(this,SignUpActivity::class.java)
             startActivity(intent)
@@ -37,6 +57,55 @@ class SignInActivity : AppCompatActivity() {
                     }
             }else{
                 Toast.makeText(this,"Empty fields are not allowed!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun signInGoogle() {
+        Log.e("LOG", "signInGoogle()")
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                Log.e("LOG", "launcher")
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResult(task)
+            } else{
+                Log.e("LOG", "resultCode: " + result.resultCode)
+                Log.e("LOG", "Activity.RESULT_OK: " + Activity.RESULT_OK)
+                Log.e("LOG", "Activity.RESULT_CANCELED: " + Activity.RESULT_CANCELED)
+                Log.e("LOG", "Activity.RESULT_FIRST_USER: " + Activity.RESULT_FIRST_USER)
+            }
+    }
+
+    private fun handleResult(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account: GoogleSignInAccount? = task.result
+            if (account != null){
+                Log.e("LOG", "Inside handleResult()")
+                updateUi(account)
+            }
+
+        }else{
+            Log.e("ERROR", task.exception.toString())
+            Toast.makeText(this,task.exception.toString(),Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun updateUi(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful){
+                Log.e("LOG","Inside updateUi, isSfuccessful")
+               // goToMain()
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, it.exception.toString(),Toast.LENGTH_LONG).show()
             }
         }
     }
